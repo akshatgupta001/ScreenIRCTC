da1ac18d57ad10ab015ac051cf208329665a4843
Akshat Gupta <akshatgupta001@live.com>
2018-07-03 12:22:28 +0530

    drag down workabale

--- screens/paytmVC.swift
+++ screens/paytmVC.swift
@@ -11,20 +11,44 @@ import UIKit
 class paytmVC: UIViewController {
     @IBOutlet weak var contentView: UIView!
    
+    @IBOutlet weak var NetBankingView: UIView!
+    @IBOutlet weak var cardImg: UIImageView!
+    @IBOutlet weak var cardView: UIView!
     
- 
+    @IBOutlet weak var cardPayment: UIView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let tap = UITapGestureRecognizer(target: self, action: #selector(creditTap))
+        cardView.addGestureRecognizer(tap)
+         NetBankingView.frame = CGRect(x: 8, y: 210, width: view.frame.width - 16, height: 100)
         
-       
-        
-        contentView.heightAnchor.constraint(equalToConstant: 700)
         setNavBar()
       
     }
+    func dragNetbankDown(){
+        NetBankingView.frame = CGRect(x: 8, y: 440, width: view.frame.width-16, height: 100)
+        NetBankingView.isHidden = false;
+    }
   
-   
+   @objc func creditTap(){
+    if cardImg.image == #imageLiteral(resourceName: "ic_radiooff"){
+       
+        dragNetbankDown()
+
+        cardPayment.isHidden = false
+        cardImg.image = #imageLiteral(resourceName: "ic_radio_on")
+       
+    }else{
+        cardPayment.isHidden = true
+        cardImg.image = #imageLiteral(resourceName: "ic_radiooff")
+         NetBankingView.frame = CGRect(x: 8, y: 210, width: view.frame.width-16, height: 100)
+        
+    }
+    
+    }
     func setNavBar(){
         
        //self.navigationController?.navigationBar.barTintColor = UIColor.gray
@@ -36,9 +60,13 @@ class paytmVC: UIViewController {
         self.navigationItem.titleView = header
     }
     
+    
+    
     deinit {
         self.navigationController?.navigationBar.barTintColor = UIColor(displayP3Red: 0/255.0, green: 150/255.0, blue: 177/255.0, alpha: 1)
 
     }
+    
+    
 
 }
